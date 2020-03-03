22ff3a579652e67e989507faeffc79245c48dc4c
Julien Balestrieri <julien.balestrieri@gmail.com>
2020-03-03 12:34:53 +0100

    Add attribute comparison req

---
+++ Scripts/Skills/Requirements/AttributeComparisonReq.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class AttributeComparisonReq : IRequirement
+{
+    public enum ComparisonType
+    {
+        Equal,
+        NotEqual,
+        GreaterThan,
+        GreaterThanOrEqual,
+        LesserThan,
+        LesserThanOrEqual,
+    }
+
+    int _attributeType;
+    float _threshold;
+    ComparisonType _comparisonType;
+
+    public AttributeComparisonReq(int p_attributeType, float p_threshold, ComparisonType p_comparisonType)
+    {
+        _attributeType = p_attributeType;
+        _threshold = p_threshold;
+        _comparisonType = p_comparisonType;
+    }
+    
+	public bool IsValid(GameObject p_owner)
+    {
+        Attribute<float> attribute = p_owner.GetComponent<AttributeManager>().GetAttribute<float>(_attributeType);
+        
+        bool isValid = false;
+        switch (_comparisonType)
+        {
+            case ComparisonType.Equal:
+                isValid = attribute.Value == _threshold;
+            break;
+            case ComparisonType.NotEqual:
+                isValid = attribute.Value != _threshold;
+            break;
+            case ComparisonType.GreaterThan:
+                isValid = attribute.Value > _threshold;
+            break;
+            case ComparisonType.GreaterThanOrEqual:
+                isValid = attribute.Value >= _threshold;
+            break;
+            case ComparisonType.LesserThan:
+                isValid = attribute.Value < _threshold;
+            break;
+            case ComparisonType.LesserThanOrEqual:
+                isValid = attribute.Value <= _threshold;
+            break;
+            default:
+            throw new System.NotImplementedException();
+
+        }
+
+        return isValid;
+	}
+}
